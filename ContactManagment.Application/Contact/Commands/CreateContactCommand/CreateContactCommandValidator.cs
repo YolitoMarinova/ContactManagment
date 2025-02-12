d01eaefa6aaeeb9cbec318f7075f95024d9c340d
Yoana Marinova <47632705+YolitoMarinova@users.noreply.github.com>
2025-02-12 14:38:19 +0200

    add fluent validation

---
+++ ContactManagment.Application/Contact/Commands/CreateContactCommand/CreateContactCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using ContactManagment.Domain.Common.Constants;
+using ContactManagment.Domain.Common.Errors;
+using FluentValidation;
+
+namespace ContactManagment.Application.Contact.Commands.CreateContactCommand
+{
+    public class CreateContactCommandValidator: AbstractValidator<CreateContactCommand>
+    {
+        public CreateContactCommandValidator()
+        {
+            RuleFor(c => c.FirstName).NotEmpty().NotNull().MaximumLength(ContactConstants.FirstNameMaxLength)
+                .WithMessage(string.Format(ContactErrorMessages.InvalidFirstName, ContactConstants.FirstNameMaxLength));
+            RuleFor(c => c.SurName).NotEmpty().NotNull().MaximumLength(ContactConstants.SurNameMaxLength)
+                .WithMessage(string.Format(ContactErrorMessages.InvalidSurName, ContactConstants.SurNameMaxLength));
+            RuleFor(c => c.Address).NotEmpty().NotNull().MaximumLength(ContactConstants.AddressMaxLength)
+                .WithMessage(string.Format(ContactErrorMessages.InvalidAddress, ContactConstants.AddressMaxLength));
+            RuleFor(c => c.PhoneNumber).NotEmpty().NotNull().MaximumLength(ContactConstants.PhoneNumberMaxLength)
+                .WithMessage(string.Format(ContactErrorMessages.InvalidPhoneNumber, ContactConstants.PhoneNumberMaxLength));
+            RuleFor(c => c.Iban).NotEmpty().NotNull().MinimumLength(ContactConstants.IBANMinLength).MaximumLength(ContactConstants.IBANMaxLength)
+                .WithMessage(string.Format(ContactErrorMessages.InvalidIBAN, ContactConstants.IBANMinLength, ContactConstants.IBANMaxLength));
+        }
+    }
+}
---
+++ ContactManagment.Application/Contact/Commands/UpdateContactCommand/UpdateContactCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using ContactManagment.Domain.Common.Constants;
+using ContactManagment.Domain.Common.Errors;
+using FluentValidation;
+
+namespace ContactManagment.Application.Contact.Commands.UpdateContactCommand
+{
+    public class UpdateContactCommandValidator : AbstractValidator<UpdateContactCommand>
+    {
+        public UpdateContactCommandValidator()
+        {
+            RuleFor(c => c.FirstName)
+                .NotEmpty().NotNull().MaximumLength(ContactConstants.FirstNameMaxLength)
+                .When(c => c.FirstName != null)
+                .WithMessage(string.Format(ContactErrorMessages.InvalidFirstName, ContactConstants.FirstNameMaxLength));
+            RuleFor(c => c.SurName)
+                .NotEmpty().NotNull().MaximumLength(ContactConstants.SurNameMaxLength)
+                .When(c => c.SurName != null)
+                .WithMessage(string.Format(ContactErrorMessages.InvalidSurName, ContactConstants.SurNameMaxLength));
+            RuleFor(c => c.Address)
+                .NotEmpty().NotNull().MaximumLength(ContactConstants.AddressMaxLength)
+                .When(c => c.Address != null)
+                .WithMessage(string.Format(ContactErrorMessages.InvalidAddress, ContactConstants.AddressMaxLength));
+            RuleFor(c => c.PhoneNumber)
+                .NotEmpty().NotNull().MaximumLength(ContactConstants.PhoneNumberMaxLength)
+                .When(c => c.PhoneNumber != null)
+                .WithMessage(string.Format(ContactErrorMessages.InvalidPhoneNumber, ContactConstants.PhoneNumberMaxLength));
+            RuleFor(c => c.Iban)
+                .NotEmpty().NotNull().MinimumLength(ContactConstants.IBANMinLength).MaximumLength(ContactConstants.IBANMaxLength)
+                .When(c => c.Iban != null)
+                .WithMessage(string.Format(ContactErrorMessages.InvalidIBAN, ContactConstants.IBANMinLength, ContactConstants.IBANMaxLength));
+        }
+    }
+}
