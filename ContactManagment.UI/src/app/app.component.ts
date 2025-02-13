8707fda8f3eb48fa8a620215c1e7237271103654
Yoana Marinova <47632705+YolitoMarinova@users.noreply.github.com>
2025-02-13 20:48:56 +0200

    fix imports + clean

--- ContactManagment.UI/src/app/app.component.ts
+++ ContactManagment.UI/src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { Contact } from './models/contact.model';
-import { Observable } from 'rxjs/internal/Observable';
-import { AppState } from './store/app.state';
-import { Store } from '@ngrx/store';
-import * as ContactActions from './store/contact/contact.actions';
 
 @Component({
   selector: 'app-root',
@@ -12,31 +7,4 @@ import * as ContactActions from './store/contact/contact.actions';
 })
 export class AppComponent {
   title = 'ContactManagment.UI';
-
-  contacts$: Observable<Contact[]>;
-
-  constructor(private store: Store<AppState>) {
-    this.contacts$ = store.select(state => state.contacts.contacts);
-  }
-
-  loadContacts() {
-    this.store.dispatch(ContactActions.loadContacts());
-  }
-
-  addContact() {
-    const newContact: Contact = {
-      id: crypto.randomUUID(), // Generate a random ID for testing
-      firstName: 'John',
-      surName: 'Doe',
-      dateOfBirth: '1990-01-01',
-      address: '123 Street',
-      phoneNumber: '1234567890',
-      iban: 'IBAN12345'
-    };
-    this.store.dispatch(ContactActions.addContact({ contact: newContact }));
-  }
-
-  deleteContact(id: string) {
-    this.store.dispatch(ContactActions.deleteContact({ id }));
-  }
 }
--- ContactManagment.UI/src/app/app.module.ts
+++ ContactManagment.UI/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpClientModule } from '@angular/common/http';
-import { contactReducer } from './state/contact/contact.reducer';
-import { ContactEffects } from './state/contact/contact.effects';
+import { contactReducer } from './store/contact/contact.reducer';
+import { ContactEffects } from './store/contact/contact.effects';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
